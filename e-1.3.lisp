9310889af2ade963eb1a005e8ed684e571db59d4
finico <git.alexey@gmail.com>
2013-11-03 15:15:05 +0300

    issue #1

--- e-1.3.lisp
+++ e-1.3.lisp
@@ -1,10 +1,29 @@
-(define (square-max a b c)
-    (cond ( (and (> a b) (> a c) ) a)
-          ((> b c) b)
-          (else c)
+(define (sum-of-squares-max a b c)
+    
+    (define (sum-of-squares x y)
+        (+ (* x x) (* y y))
+    )
+
+    (cond
+        ((and (> a b) (> a c) ) 
+            (if (> b c)
+                (sum-of-squares a b)
+                (sum-of-squares a c)
+            )
+        )
+        ((> a c)
+            (if (> b c)
+                (sum-of-squares b a)
+                (sum-of-squares b c)
+            )
+        )
+        (else
+            (if (> a b)
+                (sum-of-squares c a)
+                (sum-of-squares c b)
+            )
+        )
     )
 )
 
-(square-max 8 5 9)
-
-; оценка 100%
+(sum-of-squares-max 1 5 3)
