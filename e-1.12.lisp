b1d58b84133703ccbd070472ebb6abc090a2e9dd
finico <finico.a@gmail.com>
2014-01-13 21:06:29 +0300

    Update e-1.12.lisp

--- e-1.12.lisp
+++ e-1.12.lisp
@@ -1,6 +1,4 @@
 (define (triangle y x)
-    (if (or (> x y) (< y 1))
-        0
-        (if (or (= x 1) (= x y))
-            1
-            (+ (triangle (- y 1) (- x 1)) (triangle (- y 1) x)))))
+    (cond ((or (> x y) (< y 1)) 0)
+          ((or (= x 1) (= x y)) 1)
+          (else (+ (triangle (- y 1) (- x 1)) (triangle (- y 1) x)))))
